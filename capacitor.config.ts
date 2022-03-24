89a01d108b1a98824aee7e4b7ec9fc1c68947a02
manyNeurons <ferranramirezmarti@gmail.com>
2022-03-24 12:59:41 +0100

    name changed to zaaax

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ionic-video-analyzer',
+  appName: 'zaax-video-analyzer',
   webDir: 'www',
   bundledWebRuntime: false
 };
