591dad21e4598c290870a555f629af8f3ba495e1
fewNeurons <ferranramirezmarti@gmail.com>
2022-05-10 11:55:45 +0200

    ios rehecho el proyect

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.zaax.video_analyzer_ios',
+  appId: 'com.zaaxapp.zaax',
   appName: 'zaax-video-analyzer',
   webDir: 'www',
   bundledWebRuntime: false
